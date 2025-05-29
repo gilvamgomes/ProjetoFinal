bac4c08599668eea12ba8b7aac6d92bc097ed1cb
weverttongg <weverttonrodrigues09@gmail.com>
2025-05-28 21:16:14 -0300

    Update Feria.java

--- src/java/model/Ferias.java
+++ src/java/model/Ferias.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author wever
- */
+import java.util.Date;
+
 public class Ferias {
-    
+    private int idFerias;
+    private Date dataInicio;
+    private Date dataFim;
+    private String status;
+    private int funcionario_idFfuncionario;
+
+    public int getIdFerias() {
+        return idFerias;
+    }
+
+    public void setIdFerias(int idFerias) {
+        this.idFerias = idFerias;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(Date dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(Date dataFim) {
+        this.dataFim = dataFim;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getFuncionario_idFfuncionario() {
+        return funcionario_idFfuncionario;
+    }
+
+    public void setFuncionario_idFfuncionario(int funcionario_idFfuncionario) {
+        this.funcionario_idFfuncionario = funcionario_idFfuncionario;
+    }
 }
